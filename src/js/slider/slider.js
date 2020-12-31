1d5de950a645ecf2f92d59737a13a9c8b6015d8f
Andreyz11288 <andreyz11288@gmail.com>
2020-12-31 17:50:41 +0200

    отзывчивый слайдер

--- src/js/slider/slider.js
+++ src/js/slider/slider.js
@@ -2,14 +2,34 @@ import Swiper from 'swiper/bundle';
 export default function onSlider() {
   document.querySelectorAll('.swiper-container').forEach(function (elem) {
     	new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      slidesPerGroup: 3,
+      slidesPerView: 2,
+      spaceBetween: 20,
+      // slidesPerGroup: 3,
       loop: true,
       loopFillGroupWithBlank: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
+      },
+      breakpoints: {
+        // when window width is >= 740px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          direction: 'horizontal',
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          direction: 'horizontal',
+          slidesPerGroup: 2,
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+          direction: 'horizontal',
+          slidesPerGroup: 4,
+        },
       },
       navigation: {
         nextEl: '.swiper-button-next',
