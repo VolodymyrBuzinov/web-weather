6c24037fff2ca2d51c65e24032d16dd21d2d44ef
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-01-03 18:24:44 +0200

    add button logic

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,10 @@ const mainRefs = {
 }
 mainRefs.header.addEventListener('click', evt => {
     evt.preventDefault();    
-    headerLogic.historyApi(evt);    
-    // console.log(evt.target);     
+    headerLogic.historyApi(evt);       
+    headerLogic.searchQuery(evt);        
 })
-mainRefs.header.addEventListener('change', evt => {
-    evt.preventDefault();       
-    headerLogic.searchQuery(evt);
-    // console.log(evt.target);     
-})
+
 mainRefs.main.addEventListener('click', evt => {
-    evt.preventDefault();
-    // console.log(evt.target);
+    evt.preventDefault();  
 })
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export default {   
     navLinks: document.querySelectorAll('.header-nav__link'),
     headerButton: document.querySelector('.header-button'),
+    headerInput: document.querySelector('.header-input'),
 }
