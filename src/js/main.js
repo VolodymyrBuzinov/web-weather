f71aae36cf4b9e4869bd91359eb6661a80fc092f
Andreyz11288 <andreyz11288@gmail.com>
2021-01-03 14:25:46 +0200

    при нажатии на "фото" отображается секция слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,33 @@
+import refs from './refs'
+import HeaderLogic from './header/headerLogic'
 import bigImg from './slider/bigImg'
 
+const headerLogic = new HeaderLogic();
 const mainRefs = {
     header: document.querySelector('.header'),
     main: document.querySelector('.main'),
 }
 mainRefs.header.addEventListener('click', evt => {
-    console.log(evt);
+    evt.preventDefault();    
+    headerLogic.historyApi(evt);    
+
+    if (evt.target.getAttribute('href') === "/photo"){
+        const hiddenSlider = document.querySelector('#slider')
+        hiddenSlider.classList.remove('is-hidden')
+    }
+    // console.log(evt.target);     
+})
+mainRefs.header.addEventListener('change', evt => {
+    evt.preventDefault();       
+    headerLogic.searchQuery(evt);
+    // console.log(evt.target);     
 })
 mainRefs.main.addEventListener('click', evt => {
-    console.log(evt);
-    
+    evt.preventDefault();
+     
     if (evt.target.getAttribute('class') === "photo swiper-slide") {
        
         bigImg(evt)
     }
+    // console.log(evt.target);
 })
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
-const refs = {
-    body: document.querySelector('.main'),
+export default  {   
+    navLinks: document.querySelectorAll('.header-nav__link'),
+    headerButton: document.querySelector('.header-button'),
+    body: document.querySelector('.swiper-wrapper'),
 }
-export default refs;
