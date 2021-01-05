2bf548aac2d11fa73290193eb840c65a34f579c3
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-01-05 21:46:19 +0200

    Добавил логику фоток

--- src/js/main.js
+++ src/js/main.js
@@ -3,61 +3,43 @@ import HeaderLogic from './header/headerLogic';
 import mapsMarkup from '../templates/maps.hbs';
 import MyMap from './maps/maps';
 import SliderLogic from './slider/slider'
-
-
 const headerLogic = new HeaderLogic();
 const myMap = new MyMap();
 const sliderLogic = new SliderLogic();
-
 const mainRefs = {
   header: document.querySelector('.header'),
   main: document.querySelector('.main'),
 };
-
-
 mainRefs.header.addEventListener('click', evt => {
   evt.preventDefault();
   headerLogic.historyApi(evt);
   headerLogic.searchQuery(evt);
-
   if (evt.target.getAttribute('href') === '/maps') {
     mainRefs.main.innerHTML = '';
     mainRefs.main.innerHTML = mapsMarkup();
     myMap.initializeMap();
     myMap.codeAddress();
   }
-  
   if (evt.target.classList.contains('header-button')) {
     const location = window.location.href.split('/');
     if (location[location.length - 1] === 'photo') {
       sliderLogic.inputSearchValue(refs.headerInput.value)
-    }
-
-    if (evt.target.classList.contains('header-button')) {
-      const location = window.location.href.split('/');
-
+    }    
       if (location[location.length - 1] !== 'maps') {
         return;
       }
       myMap.codeAddress();
     }
-  
     if (evt.target.getAttribute('href') === "/photo") {
-    
-      sliderLogic.inputSearchValue( headerLogic.searchQuery(evt))
-    }
-  }
+      sliderLogic.inputSearchValue(refs.headerInput.value)
+    }  
 })
-  
-  
  mainRefs.main.addEventListener('click', evt => {
   if (evt.target.dataset.layer) {
     evt.preventDefault();
     myMap.eventListenerOnButtons(evt);
   }
-
   if (evt.target.getAttribute('class') !== "photo") {
     evt.preventDefault();       
   }
-
 })
