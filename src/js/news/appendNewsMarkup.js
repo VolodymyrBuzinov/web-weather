6ff1b5dc54c0952cfba817f149ec141f6f7398eb
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-01-28 13:50:50 +0200

    add news fix

--- src/js/news/appendNewsMarkup.js
+++ src/js/news/appendNewsMarkup.js
@@ -1,7 +1,7 @@
 import refs from '../refs'
 import './fetchNews'
 import newsHbs from '../../templates/news.hbs'
-
+import dftImg from '../../images/default-img.gif'
 export default class AppendNewsMarkup {
     constructor(main, fetchNews){
         this.refs = {
@@ -22,7 +22,7 @@ export default class AppendNewsMarkup {
  
     appendMarkup(news){
         const { articles } = news;
-        articles.map( article => {
+        articles.map(article => {                      
             if (article.description === '') {
                 article.description = `Детали читайте на сайте ${article.source.name}.`;
             }
--- src/js/news/fetchNews.js
+++ src/js/news/fetchNews.js
@@ -2,13 +2,18 @@ export default class FetchNews {
     constructor () {}
 
     getLastNews (){
-        const url = 'http://newsapi.org/v2/top-headlines?' +
-        'country=ua&' +
-        'apiKey=dcd86c1488e4473badee8250e13e81e8';
+        // const url = 'http://newsapi.org/v2/everything?q=Климат&from=2021-01-28&sortBy=popularity&apiKey=dcd86c1488e4473badee8250e13e81e8';        
+       const url = 'http://newsapi.org/v2/everything?' +
+           'q=Климат&' +
+           'language=ru&' +          
+          'apiKey=dcd86c1488e4473badee8250e13e81e8';
         let req = new Request(url);
         return fetch(req)
             .then(res => res.json(), res => res.status)
-            .then(news => { return news })
+            .then(news => {
+                console.log(news);
+                return news
+            })
             .catch(reason => console.error(reason))
     }
 }
